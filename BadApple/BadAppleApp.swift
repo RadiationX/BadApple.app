248a66337430a3181437aeea90c2bcb2c9d5f824
RadiationX <radiationx@yandex.ru>
2024-07-21 21:01:31 +0500

    Initial Commit

---
+++ BadApple/BadAppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BadAppleApp.swift
+//  BadApple
+//
+//  Created by RadiationX on 21.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct BadAppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
